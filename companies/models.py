34060547e66f8b7bf2c574d9319720ed48b9f514
Fabio Jesus da Silva Mattes <fabio_mattes@sicredi.com.br>
2024-08-13 16:32:16 -0300

    Adicionado models Task e TaskStatus

--- companies/models.py
+++ companies/models.py
@@ -8,3 +8,20 @@ class Enterprise(models.Model):
 class Employee(models.Model):
     user = models.ForeignKey(to="accounts.User", on_delete=models.CASCADE)
     enterprise = models.ForeignKey(to=Enterprise, on_delete=models.CASCADE)
+
+class TaskStatus(models.Model):
+    name = models.CharField(max_length=155)
+    codename = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = "company_task_status"
+        
+class Task(models.Model):
+    title = models.TextField()
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    status = models.ForeignKey(to=TaskStatus, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(to=Enterprise, on_delete=models.CASCADE)
+    employee = models.ForeignKey(to=Employee, on_delete=models.CASCADE)
