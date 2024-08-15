ac3b24c755e5f219e942affb3a7328dd3be846f7
Fábio Mattes <fabiomattes2007@gmail.com>
2024-08-15 20:23:00 -0300

    Permissions views and endpoints

--- companies/urls.py
+++ companies/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from companies.views.employees import *
+from companies.views.permissions import *
 
 urlpatterns = [
     # Employess endpoints
     path('employees', Employees.as_view()),
     path('employees/<int:employee_id>', EmployeeDetail.as_view()),
+
+    # Groups & Permissions endpoints
+    path('permissions', PermissionDetail.as_view()),
 ]
